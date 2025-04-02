5b84330f76e70792f24d53514284531ce4641cac
Maryna Maksimuk <maksimarina@gmail.com>
2025-04-02 13:17:43 -0500

    Base project

---
+++ store/src/main/java/com/my_new_project/store/StoreApplication.java
@@ -0,0 +1,13 @@
+package com.my_new_project.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
---
+++ store/src/test/java/com/my_new_project/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_new_project.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
